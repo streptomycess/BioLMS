27af16fa34473c45b5f4e9368702905da255d949
Julia Khomyakova <khomyakova.julia.37@gmail.com>
2026-04-01 21:47:54 +0300

    Add home page

--- biology/urls.py
+++ biology/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('lesson/<int:lesson_id>/', views.lesson_detail, name='lesson_detail'),
     path('lesson/<int:lesson_id>/quiz/', views.lesson_quiz, name='lesson_quiz'),
 ]
--- biology/views.py
+++ biology/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Lesson
 
+def home(request):
+    lessons = Lesson.objects.all()
+    return render(request, 'biology/home.html', {'lessons': lessons})
+
 def lesson_detail(request, lesson_id):
-    """Страница с видеоуроком"""
     lesson = get_object_or_404(Lesson, id=lesson_id)
     return render(request, 'biology/lesson_detail.html', {'lesson': lesson})
 
 def lesson_quiz(request, lesson_id):
-    """Страница квиза с автопроверкой"""
     lesson = get_object_or_404(Lesson, id=lesson_id)
     questions = lesson.flashcards.all()
     score = None
@@ -15,7 +17,6 @@ def lesson_quiz(request, lesson_id):
     if request.method == "POST":
         correct_answers = 0
         for q in questions:
-            # Получаем ответ пользователя из формы по ID вопроса
             user_answer = request.POST.get(str(q.id), "").strip().lower()
             correct_answer = q.answer.strip().lower()
             if user_answer == correct_answer:
