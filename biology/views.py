dabfa30e990b376a335bba76ad115befd9fcb602
Julia Khomyakova <khomyakova.julia.37@gmail.com>
2026-04-01 21:40:47 +0300

    Add logic pass/fail

--- biology/views.py
+++ biology/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Lesson
 
-# Create your views here.
+def lesson_detail(request, lesson_id):
+    """Страница с видеоуроком"""
+    lesson = get_object_or_404(Lesson, id=lesson_id)
+    return render(request, 'biology/lesson_detail.html', {'lesson': lesson})
+
+def lesson_quiz(request, lesson_id):
+    """Страница квиза с автопроверкой"""
+    lesson = get_object_or_404(Lesson, id=lesson_id)
+    questions = lesson.flashcards.all()
+    score = None
+
+    if request.method == "POST":
+        correct_answers = 0
+        for q in questions:
+            # Получаем ответ пользователя из формы по ID вопроса
+            user_answer = request.POST.get(str(q.id), "").strip().lower()
+            correct_answer = q.answer.strip().lower()
+            if user_answer == correct_answer:
+                correct_answers += 1
+        score = f"Вы набрали {correct_answers} из {questions.count()}!"
+
+    return render(request, 'biology/quiz.html', {'lesson': lesson, 'questions': questions, 'score': score})
